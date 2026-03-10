d755256d01de5a350649e05d2a6917bb1db2bb5c
TheWoodenPuppet <vol1shaikh@gmail.com>
2026-03-10 16:36:22 +0530

    Feature link formatter complete

--- config.py
+++ config.py
@@ -15,12 +15,11 @@ TIMEZONE = pytz.timezone("Asia/Kolkata")
 
 # DAILY MESSAGES
 DAILY_CHAT_IDS = [
-    # -1002511165129, # Notebook of Deku
-    -1002911938910, # disappearing notes
+    -1002511165129, # Notebook of Deku
 ]
 
 # FOREST SESSION
-SESSION_CHAT_ID = -1002511165129 # Notebook of Deku
+# SESSION_CHAT_ID = -1002511165129 # Notebook of Deku
 SESSION_USER_ID = 1463187459 # Deku
-
+SESSION_CHAT_ID = -1002911938910 # disappearing
 
