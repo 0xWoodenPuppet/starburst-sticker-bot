44470bd2293539d3a3a8ff1939c27bdf76a0c0eb
TheWoodenPuppet <vol1shaikh@gmail.com>
2026-04-19 21:49:06 +0530

    Alternate sticker for /sleep

--- handlers/sleep.py
+++ handlers/sleep.py
@@ -6,9 +6,16 @@ async def handle_sleep(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message or not update.message.reply_to_message:
         return
 
+    original_message = update.message.reply_to_message
+
+    # Default sticker
     sticker_id = "CAACAgQAAxkBAAEQ9FBp5PY37uGxIhdcPtPkydXh2WlMdgAC7hoAAk3CGVPqbt18v28DSjsE"
     
-    await update.message.reply_to_message.reply_sticker(
+    # If the original message was sent by the specified user
+    if original_message.from_user and original_message.from_user.id == 1463187459:
+        sticker_id = "CAACAgQAAxkBAAEQ9GRp5P1RZ_BLF-bMsxH5tQABLIA_sjoAAuwYAAI3TMFTcnY6q7oxvJw7BA"
+    
+    await original_message.reply_sticker(
         sticker=sticker_id,
         disable_notification=True
     )
